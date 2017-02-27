0c9c372e121c8cac00e2434fe10e52a2459d6933
Arpan <arpann2@gmail.com>
2017-02-27 01:13:17 +0000

    updated TestNG1 class

--- src/test/java/TestNG1.java
+++ src/test/java/TestNG1.java
@@ -1,9 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by arpan on 26/02/2017.
  */
 public class TestNG1 {
-    public static void main(String[] args) {
+    String firefoxPath = "E:\\JARs\\Selenium-3.0.1\\geckodriver-v0.11.1-win64\\geckodriver.exe";
+    WebDriver driver;
 
-        System.out.println("hello world");
-    }
+        @BeforeMethod
+        public void init() {
+            System.setProperty("webdriver.gecko.driver", firefoxPath);
+            driver = new FirefoxDriver();
+        }
+
+        @Test
+        public void openBrowser(){
+            driver.get("http:/www.bbc.com");
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
+        @AfterMethod
+        public void tearDown() {
+            driver.quit();
+        }
+
+
 }
